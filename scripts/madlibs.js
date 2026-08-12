20cd4a3e05afb432af7565494ab661b41f8dbd33
CharityCP <perkinscharity78@gmail.com>
2026-08-11 20:03:17 -0500

    mad libs v0 asks and answers

---
+++ scripts/madlibs.js
@@ -0,0 +1,20 @@
+let name = prompt("A name:");
+let adjective = prompt("An adjective:");
+let noun = prompt("A noun:");
+
+console.log("Dear " + name + ",");
+console.log("You are as " + adjective + " as a " + noun + ".");
+console.log("Happy birthday from the generator.");
+
+A name: Ada
+An adjective: brave
+A noun: volcano
+Dear Ada,
+You are as brave as a volcano.
+Happy birthday from the generator.
+
+function makeCard() {
+  console.log("Dear Ada,");
+}
+
+makeCard();
