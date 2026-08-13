bb438e18206c16563019a3d06023398600edc8ce
CharityCP <perkinscharity78@gmail.com>
2026-08-12 20:19:57 -0500

    case-proof the style question

--- scripts/madlibs-working.js
+++ scripts/madlibs-working.js
@@ -17,7 +17,8 @@ function randomNumber(a, b) {
 
 // Computer Output
 function makeCard() {
-  if (style === "fancy") {
+ if (style.toLowerCase() === "fancy") {
+
     console.log("Dearest " + name + ",");
   } else {
     console.log("YO " + name + "!");
