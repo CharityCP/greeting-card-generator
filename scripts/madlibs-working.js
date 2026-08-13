f4db5dad84835b5015227492d231c9d11dcb5278
CharityCP <perkinscharity78@gmail.com>
2026-08-12 20:11:24 -0500

    Updated to add style function

--- scripts/madlibs-working.js
+++ scripts/madlibs-working.js
@@ -1,11 +1,31 @@
+//user Input
 let name = prompt("A name:");
 let adjective = prompt("An adjective:");
 let noun = prompt("A noun:");
+let style = prompt("Fancy or silly?");
 
+
+let closings = [
+  "You are a legend.",
+  "Stay golden. Silly Dilly",
+  "Signs point to awesome.",
+];
+
+function randomNumber(a, b) {
+  return Math.floor(Math.random() * (b - a + 1)) + a;
+}
+
+// Computer Output
 function makeCard() {
-  console.log("Dear " + name + ",");
-  console.log("You are as " + adjective + " as a " + noun + ".");
-  console.log("Happy birthday from the generator.");
+  if (style === "fancy") {
+    console.log("Dearest " + name + ",");
+  } else {
+    console.log("YO " + name + "!");
+  }
+  // ...rest of the card unchanged
+
+   let index = randomNumber(0, closings.length - 1);
+  console.log(closings[index]);
 }
 
 makeCard();
