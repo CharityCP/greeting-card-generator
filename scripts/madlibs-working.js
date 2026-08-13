be563a6c36a21d1089515d9e3088067eeb4919ff
CharityCP <perkinscharity78@gmail.com>
2026-08-13 06:05:10 -0500

    fixing unidentified string

--- scripts/madlibs-working.js
+++ scripts/madlibs-working.js
@@ -23,7 +23,7 @@ function makeCard() {
   } else {
     console.log("YO " + name + "!");
   }
- console.log("Hello" + name + " You are a Silly dilly"");
+ console.log("Hello" + name + " You are a Silly dilly");
  
    let index = randomNumber(0,2);
   console.log(closings[index]);
