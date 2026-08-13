55101bc7295f5d88ed09f2bc47fab9bcd5d42131
CharityCP <perkinscharity78@gmail.com>
2026-08-12 21:33:06 -0500

    added comment

--- scripts/madlibs-working.js
+++ scripts/madlibs-working.js
@@ -10,7 +10,7 @@ let closings = [
   "Stay golden. Silly Dilly",
   "Signs point to awesome.",
 ];
-
+//helper function
 function randomNumber(a, b) {
   return Math.floor(Math.random() * (b - a + 1)) + a;
 }
