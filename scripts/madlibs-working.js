efdb99e012831987c32aaddf41c42f342e6aad9e
CharityCP <perkinscharity78@gmail.com>
2026-08-13 05:47:32 -0500

    add to  madlib to make code work

--- scripts/madlibs-working.js
+++ scripts/madlibs-working.js
@@ -23,8 +23,7 @@ function makeCard() {
   } else {
     console.log("YO " + name + "!");
   }
-  // ...rest of the card unchanged
-
+ 
    let index = randomNumber(0, closings.length - 1);
   console.log(closings[index]);
 }
